c4cf391f31bc3b28df91daba9fd7d9bc1353acaa
Yves Goergen <yves@unclassified.de>
2014-01-25 19:44:32 +0100

    TxEditor wizard: Parameter detection in selected code

--- TxEditor/View/TextKeyWizardWindow.xaml.cs
+++ TxEditor/View/TextKeyWizardWindow.xaml.cs
@@ -30,7 +30,9 @@ namespace TxEditor.View
 		#region Private data
 
 		private string exactMatchTextKey;   // TODO: remove
-		private string initialText;
+		private string initialClipboardText;
+		private string parsedText;
+		private List<PlaceholderData> placeholders = new List<PlaceholderData>();
 		private int keySuggestPhase;   // TODO: remove?
 		private List<SuggestionViewModel> suggestions = new List<SuggestionViewModel>();
 
@@ -71,8 +73,8 @@ namespace TxEditor.View
 			}
 			TextOptions.SetTextFormattingMode(TranslationText, MainWindowViewModel.Instance.TextFormattingMode);
 
-			initialText = ReadClipboard();
-			if (initialText == null)
+			initialClipboardText = ReadClipboard();
+			if (initialClipboardText == null)
 			{
 				Close();
 				return;
@@ -147,7 +149,10 @@ namespace TxEditor.View
 		private void SourceCodeCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			SetDefaultCheckbox.Visibility =
-				SourceCodeCombobox.SelectedItem as string == "XAML" ? Visibility.Visible : Visibility.Hidden;
+				SourceCodeCombobox.SelectedItem as string == "XAML" ? Visibility.Visible : Visibility.Collapsed;
+			
+			// Re-evaluate the format and parameters
+			ResetButton_Click(null, null);
 		}
 
 		private void TextKeyText_TextChanged(object sender, TextChangedEventArgs e)
@@ -189,7 +194,8 @@ namespace TxEditor.View
 					MessageBoxImage.Warning);
 				return;
 			}
-			if (TranslationText.Text != initialText &&
+			// TODO: Find another source than parsedText, use the actual text key content instead
+			if (TranslationText.Text != parsedText &&
 				MainWindowViewModel.Instance.TextKeys.ContainsKey(textKey))
 			{
 				TaskDialogResult result = TaskDialog.Show(
@@ -211,8 +217,24 @@ namespace TxEditor.View
 
 				string keyString = textKey.Replace("\\", "\\\\").Replace("\"", "\\\"");
 
-				string code = "Tx.T(\"" + keyString + "\")";
-				Clipboard.SetText(code);
+				StringBuilder codeSb = new StringBuilder();
+				codeSb.Append("Tx.T(\"" + keyString + "\"");
+				foreach (var pd in placeholders)
+				{
+					codeSb.Append(", \"" + pd.Name.Replace("\\", "\\\\").Replace("\"", "\\\"") + "\"");
+					codeSb.Append(", ");
+					if (pd.IsQuoted)
+					{
+						codeSb.Append("Tx.Q(");
+					}
+					codeSb.Append(pd.Code);
+					if (pd.IsQuoted)
+					{
+						codeSb.Append(")");
+					}
+				}
+				codeSb.Append(")");
+				Clipboard.SetText(codeSb.ToString());
 			}
 			if (SourceCodeCombobox.SelectedIndex == 1)   // XAML
 			{
@@ -237,7 +259,219 @@ namespace TxEditor.View
 
 		private void ResetButton_Click(object sender, RoutedEventArgs e)
 		{
-			TranslationText.Text = initialText;
+			// Detect parameters in the code
+			placeholders.Clear();
+			parsedText = "";
+			if (initialClipboardText != null)
+			{
+				if (SourceCodeCombobox.SelectedIndex == 0)   // C#
+				{
+					bool inStringLiteral = false;
+					bool isVerbatimString = false;
+					bool inCharLiteral = false;
+					int parensLevel = 0;
+					int bracketsLevel = 0;
+					int bracesLevel = 0;
+					int lastStringEnd = 0;
+					StringBuilder stringContent = new StringBuilder();
+
+					for (int pos = 0; pos < initialClipboardText.Length; pos++)
+					{
+						char ch = initialClipboardText[pos];
+						char nextChar = pos + 1 < initialClipboardText.Length ? initialClipboardText[pos + 1] : '\0';
+
+						if (!inStringLiteral && !inCharLiteral && ch == '\'')
+						{
+							// Start char literal
+							inCharLiteral = true;
+						}
+						else if (inCharLiteral && ch == '\\')
+						{
+							// Escape sequence, skip next character
+							pos++;
+						}
+						else if (inCharLiteral && ch == '\'')
+						{
+							// End char literal
+							inCharLiteral = false;
+						}
+
+						else if (!inStringLiteral && !inCharLiteral && ch == '@' && nextChar == '"')
+						{
+							// Start verbatim string literal
+							inStringLiteral = true;
+							isVerbatimString = true;
+
+							if (parensLevel == 0 && bracketsLevel == 0 && bracesLevel == 0 && pos > 0)
+							{
+								string code = initialClipboardText.Substring(lastStringEnd, pos - lastStringEnd);
+								var pd = new PlaceholderData(placeholders.Count + 1, code);
+								placeholders.Add(pd);
+								parsedText += "{" + pd.Name + "}";
+							}
+
+							// Handled 2 characters
+							pos++;
+						}
+						else if (!inStringLiteral && !inCharLiteral && ch == '"')
+						{
+							// Start string literal
+							inStringLiteral = true;
+
+							if (parensLevel == 0 && bracketsLevel == 0 && bracesLevel == 0 && pos > 0)
+							{
+								string code = initialClipboardText.Substring(lastStringEnd, pos - lastStringEnd);
+								var pd = new PlaceholderData(placeholders.Count + 1, code);
+								placeholders.Add(pd);
+								parsedText += "{" + pd.Name + "}";
+							}
+						}
+						else if (inStringLiteral && !isVerbatimString && ch == '\\')
+						{
+							// Escape sequence, skip next character
+							pos++;
+
+							switch (nextChar)
+							{
+								case '\'':
+								case '"':
+								case '\\':
+									stringContent.Append(nextChar);
+									break;
+								case '0': stringContent.Append('\0'); break;
+								case 'a': stringContent.Append('\a'); break;
+								case 'b': stringContent.Append('\b'); break;
+								case 'f': stringContent.Append('\f'); break;
+								case 'n': stringContent.Append('\n'); break;
+								case 'r': stringContent.Append('\r'); break;
+								case 't': stringContent.Append('\t'); break;
+								case 'U':
+									long value = long.Parse(initialClipboardText.Substring(pos + 1, 8), System.Globalization.NumberStyles.HexNumber);
+									//stringContent.Append();   // TODO: What does that value mean?
+									pos += 8;
+									break;
+								case 'u':
+									int codepoint = int.Parse(initialClipboardText.Substring(pos + 1, 4), System.Globalization.NumberStyles.HexNumber);
+									stringContent.Append((char) codepoint);
+									pos += 4;
+									break;
+								case 'v': stringContent.Append('\v'); break;
+								case 'x':
+									// TODO: variable length hex value!
+									break;
+							}
+						}
+						else if (inStringLiteral && isVerbatimString && ch == '"' && nextChar == '"')
+						{
+							// Escape sequence, skip next character
+							pos++;
+							stringContent.Append(nextChar);
+						}
+						else if (inStringLiteral && ch == '"')
+						{
+							// End string literal
+							inStringLiteral = false;
+							isVerbatimString = false;
+							lastStringEnd = pos + 1;
+
+							if (parensLevel == 0 && bracketsLevel == 0 && bracesLevel == 0)
+							{
+								parsedText += stringContent.ToString();
+							}
+							stringContent.Clear();
+						}
+						else if (inStringLiteral)
+						{
+							// Append character to text
+							stringContent.Append(ch);
+						}
+
+						else if (!inStringLiteral && !inCharLiteral && ch == '(')
+						{
+							parensLevel++;
+						}
+						else if (!inStringLiteral && !inCharLiteral && ch == ')')
+						{
+							if (parensLevel > 0)
+								parensLevel--;
+						}
+						else if (!inStringLiteral && !inCharLiteral && ch == '[')
+						{
+							bracketsLevel++;
+						}
+						else if (!inStringLiteral && !inCharLiteral && ch == ']')
+						{
+							if (bracketsLevel > 0)
+								bracketsLevel--;
+						}
+						else if (!inStringLiteral && !inCharLiteral && ch == '{')
+						{
+							bracesLevel++;
+						}
+						else if (!inStringLiteral && !inCharLiteral && ch == '}')
+						{
+							if (bracesLevel > 0)
+								bracesLevel--;
+						}
+					}
+					if (lastStringEnd < initialClipboardText.Length)
+					{
+						// Some non-string content is still left (parameter at the end)
+						string code = initialClipboardText.Substring(lastStringEnd);
+						var pd = new PlaceholderData(placeholders.Count + 1, code);
+						placeholders.Add(pd);
+						parsedText += "{" + pd.Name + "}";
+					}
+				}
+				if (SourceCodeCombobox.SelectedIndex == 1)   // XAML
+				{
+					parsedText = initialClipboardText;
+					// TODO: Any further processing required?
+				}
+			}
+
+			TranslationText.Text = parsedText;
+
+			ParametersLabel.Visibility = Visibility.Collapsed;
+			ParametersGrid.Visibility = Visibility.Collapsed;
+			ParametersGrid.Children.Clear();
+			if (placeholders.Count > 0)
+			{
+				ParametersLabel.Visibility = Visibility.Visible;
+				ParametersGrid.Visibility = Visibility.Visible;
+				int row = 0;
+				foreach (var pd in placeholders)
+				{
+					var localPd = pd;
+
+					TextBox nameText = new TextBox();
+					nameText.Text = pd.Name;
+					nameText.Margin = new Thickness(0, row > 0 ? 4 : 0, 0, 0);
+					nameText.LostFocus += (s, e2) => { UpdatePlaceholderName(localPd, nameText.Text); };
+					ParametersGrid.Children.Add(nameText);
+					Grid.SetRow(nameText, row);
+					Grid.SetColumn(nameText, 0);
+
+					TextBox codeText = new TextBox();
+					codeText.Text = pd.Code;
+					codeText.Margin = new Thickness(4, row > 0 ? 4 : 0, 0, 0);
+					codeText.TextChanged += (s, e2) => { localPd.Code = codeText.Text; };
+					ParametersGrid.Children.Add(codeText);
+					Grid.SetRow(codeText, row);
+					Grid.SetColumn(codeText, 1);
+
+					CheckBox quotedCheck = new CheckBox();
+					quotedCheck.Content = "Q";
+					quotedCheck.IsChecked = pd.IsQuoted;
+					quotedCheck.Margin = new Thickness(4, row > 0 ? 4 : 0, 0, 0);
+					quotedCheck.VerticalAlignment = VerticalAlignment.Center;
+					ParametersGrid.Children.Add(quotedCheck);
+					Grid.SetRow(quotedCheck, row);
+					Grid.SetColumn(quotedCheck, 2);
+
+					row++;
+				}
+			}
 		}
 
 		private void CancelButton_Click(object sender, RoutedEventArgs e)
@@ -278,20 +512,19 @@ namespace TxEditor.View
 			}
 			if (retryCount < 20)
 			{
+				// TODO: Debug message
 				MessageBox.Show("Tried reading the clipboard " + (20 - retryCount) + " times!");
 			}
-
-			Match m = Regex.Match(str, @"^""(.*)""$");
-			if (m.Success)
-			{
-				str = m.Groups[1].Value
-					.Replace("\\n", "\n")
-					.Replace("\\r", "")
-					.Replace("\\t", "    ")
-					.Replace("\\\"", "\"")
-					.Replace("\\\\", "\\");
-			}
 			return str;
+		}
+
+		private void UpdatePlaceholderName(PlaceholderData pd, string newName)
+		{
+			if (newName != pd.Name)
+			{
+				TranslationText.Text = TranslationText.Text.Replace("{" + pd.Name + "}", "{" + newName + "}");
+				pd.Name = newName;
+			}
 		}
 
 		private void ScanAllTexts(TextKeyViewModel tk)
@@ -325,5 +558,30 @@ namespace TxEditor.View
 		}
 
 		#endregion Support functions
+
+		#region Placeholder structure
+
+		private class PlaceholderData
+		{
+			public string Name;
+			public string Code;
+			public bool IsQuoted;   // TODO: Add detection/handling for this option
+
+			public PlaceholderData(int index, string code)
+			{
+				// Strip the string glue (the + operators)
+				code = code.Trim();
+				if (code.StartsWith("+"))
+					code = code.Substring(1);
+				if (code.EndsWith("+"))
+					code = code.Substring(0, code.Length - 1);
+				code = code.Trim();
+
+				Name = "param" + index;
+				Code = code;
+			}
+		}
+
+		#endregion Placeholder structure
 	}
 }
