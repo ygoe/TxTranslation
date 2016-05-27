47c2aa1827ce48eb61cd59558f6a76c0c400ca58
Polyanskiy Dmitry <demonvooda@mail.ru>
2016-05-27 10:20:21 +0300

    Added a new method UpperCaseAll which converts whole string to upper case. Also added abbreviation UA for UpperCaseAll method. Added extension UATExtension in order to use UAT.

--- TxLib/Tx.cs
+++ TxLib/Tx.cs
@@ -1660,6 +1660,18 @@ namespace Unclassified.TxLib
 			return char.ToUpper(text[0]) + text.Substring(1);
 		}
 
+		/// <summary>
+		/// Transforms the first character of a text to upper case.
+		/// </summary>
+		/// <param name="text">Text to transform.</param>
+		/// <returns></returns>
+		public static string UpperCaseAll(string text)
+		{
+			if (string.IsNullOrEmpty(text)) return text;
+			return text.ToUpper();
+		}
+        
+
 		#endregion Text conversion and decoration
 
 		#region Number formatting
@@ -2654,6 +2666,16 @@ namespace Unclassified.TxLib
 			return UpperCase(text);
 		}
 
+        /// <summary>
+        /// Abbreviation for the <see cref="UpperCaseAll"/> method.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+	    public static string UA(string text)
+        {
+            return UpperCaseAll(text);
+        }
+
 		#region N overloads
 
 		/// <summary>
@@ -2861,6 +2883,82 @@ namespace Unclassified.TxLib
 		public static string UT(string key, decimal count, Dictionary<string, string> data)
 		{
 			return U(T(key, count, data));
+		}
+
+		#endregion UT overloads
+
+		#region UAT overloads
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key)
+		{
+			return UA(T(key));
+		}
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key, int count)
+		{
+			return UA(T(key, count));
+		}
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key, decimal count)
+		{
+			return UA(T(key, count));
+		}
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key, params string[] data)
+		{
+			return UA(T(key, data));
+		}
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key, Dictionary<string, string> data)
+		{
+			return UA(T(key, data));
+		}
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key, int count, params string[] data)
+		{
+			return UA(T(key, count, data));
+		}
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key, int count, Dictionary<string, string> data)
+		{
+			return UA(T(key, count, data));
+		}
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key, decimal count, params string[] data)
+		{
+			return UA(T(key, count, data));
+		}
+
+		/// <summary>
+		/// Combined abbreviation for the UpperCase and Text methods.
+		/// </summary>
+		public static string UAT(string key, decimal count, Dictionary<string, string> data)
+		{
+			return UA(T(key, count, data));
 		}
 
 		#endregion UT overloads
--- TxLib/TxXaml.cs
+++ TxLib/TxXaml.cs
@@ -215,6 +215,66 @@ namespace Unclassified.TxLib
 	}
 
 	/// <summary>
+	/// Markup extension providing the Tx.UT method functionality.
+	/// </summary>
+	public class UATExtension : TExtension
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Initialises a new instance of the UATExtension class.
+		/// </summary>
+		public UATExtension()
+			: base()
+		{
+		}
+
+        /// <summary>
+        /// Initialises a new instance of the UATExtension class.
+        /// </summary>
+        /// <param name="key">Text key to translate.</param>
+        public UATExtension(string key)
+			: base(key)
+		{
+		}
+
+		/// <summary>
+		/// Initialises a new instance of the UATExtension class.
+		/// </summary>
+		/// <param name="key">Text key to translate.</param>
+		/// <param name="count">Count value to consider when selecting the text value.</param>
+		public UATExtension(string key, int count)
+			: base(key, count)
+		{
+		}
+
+		/// <summary>
+		/// Initialises a new instance of the UATExtension class.
+		/// </summary>
+		/// <param name="key">Text key to translate.</param>
+		/// <param name="countBinding">Binding that provides the count value to consider when selecting the text value.</param>
+		public UATExtension(string key, Binding countBinding)
+			: base(key, countBinding)
+		{
+		}
+
+		#endregion Constructors
+
+		#region Converter action
+
+		/// <summary>
+		/// Provides the T method in specialised classes.
+		/// </summary>
+		/// <returns></returns>
+		protected override Func<string, int, string> GetTFunc()
+		{
+			return Tx.UAT;
+		}
+
+		#endregion Converter action
+	}
+
+	/// <summary>
 	/// Markup extension providing the Tx.TC method functionality.
 	/// </summary>
 	public class TCExtension : TExtension
